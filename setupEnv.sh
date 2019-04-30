10aa8a59d2c954553458275b74cea372489b710e
Steve Lascos <steve.lascos@l3t.com>
2019-04-30 14:53:06 -0400

    Fixed setupEnv.sh

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,3 +1,3 @@
-export OPENCV=/home/slascos/projects/opencv/installation/OpenCV-3.4.6/
+export OPENCV=/home/hackathon/libraries/opencv-3.4.6/installation/OpenCV-3.4.6/
 export PKG_CONFIG_PATH=$OPENCV/lib/pkgconfig/:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=${OPENCV}/lib:/usr/local/lib:$LD_LIBRARY_PATH
