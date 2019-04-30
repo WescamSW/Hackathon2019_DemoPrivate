0a53bb9fd5e63d643ba993d0ea7a12a8ea7da171
Steve Lascos <steve.lascos@l3t.com>
2019-04-30 15:29:49 -0400

    fix enviro script

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,3 +1,2 @@
 export OPENCV=/home/hackathon/libraries/opencv-3.4.6/installation/OpenCV-3.4.6/
-export PKG_CONFIG_PATH=$OPENCV/lib/pkgconfig/:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=${OPENCV}/lib:/usr/local/lib:$LD_LIBRARY_PATH
